819fe2c3a831d6de52971003085596ac9f9f3499
Marqae Morris <mamorris920@gmail.com>
2024-11-24 21:06:06 -0500

    upt

---
+++ config/database.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+'url' : "mongodb+srv://angelmorris920:HNFemIvCXAjnRgpK@cluster0.x4fet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+'dbName' : "PersonalExpress"
+}
